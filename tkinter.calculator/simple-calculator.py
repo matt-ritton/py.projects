b0d8cc8245fad1d75dd94d6f5ffd82e6c77b0dc7
Matheus Ritton <ritton.matheus@gmail.com>
2022-04-05 02:36:02 -0300

    Adding commonly used variables

--- tkinter.calculator/simple-calculator.py
+++ tkinter.calculator/simple-calculator.py
@@ -76,47 +76,53 @@ def equal(): #Show the results in screen
 button_padx = 50 #Horizontal padding
 button_pady = 15 #Vertical padding
 button_font = "Lato 12 bold"
+fg_color = "white"
+bg_color = "#383B3F"
+activebg_color = "#211F22"
 
 #PS: Normally it's not possible to pass values to the function using command, so we use lambda to use functions as we know them.
-b0 = Button(root, text="0", padx=button_padx, pady=button_pady, fg="white", bg="#383B3F", activebackground="#211F22", font=((button_font)), command=lambda : click("0"))
-b1 = Button(root, text="1", padx=button_padx, pady=button_pady, fg="white", bg="#383B3F", activebackground="#211F22", font=((button_font)), command=lambda : click(1))
-b2 = Button(root, text="2", padx=button_padx, pady=button_pady, fg="white", bg="#383B3F", activebackground="#211F22", font=((button_font)), command=lambda : click(2))
-b3 = Button(root, text="3", padx=button_padx, pady=button_pady, fg="white", bg="#383B3F", activebackground="#211F22", font=((button_font)), command=lambda : click(3))
-b4 = Button(root, text="4", padx=button_padx, pady=button_pady, fg="white", bg="#383B3F", activebackground="#211F22", font=((button_font)), command=lambda : click(4))
-b5 = Button(root, text="5", padx=button_padx, pady=button_pady, fg="white", bg="#383B3F", activebackground="#211F22", font=((button_font)), command=lambda : click(5))
-b6 = Button(root, text="6", padx=button_padx, pady=button_pady, fg="white", bg="#383B3F", activebackground="#211F22", font=((button_font)), command=lambda : click(6))
-b7 = Button(root, text="7", padx=button_padx, pady=button_pady, fg="white", bg="#383B3F", activebackground="#211F22", font=((button_font)), command=lambda : click(7))
-b8 = Button(root, text="8", padx=button_padx, pady=button_pady, fg="white", bg="#383B3F", activebackground="#211F22", font=((button_font)), command=lambda : click(8))
-b9 = Button(root, text="9", padx=button_padx, pady=button_pady, fg="white", bg="#383B3F", activebackground="#211F22", font=((button_font)), command=lambda : click(9))
+b0 = Button(root, text="0", padx=button_padx, pady=button_pady, fg=fg_color, bg=bg_color, activebackground=activebg_color, font=((button_font)), command=lambda : click("0"))
+b1 = Button(root, text="1", padx=button_padx, pady=button_pady, fg=fg_color, bg=bg_color, activebackground=activebg_color, font=((button_font)), command=lambda : click(1))
+b2 = Button(root, text="2", padx=button_padx, pady=button_pady, fg=fg_color, bg=bg_color, activebackground=activebg_color, font=((button_font)), command=lambda : click(2))
+b3 = Button(root, text="3", padx=button_padx, pady=button_pady, fg=fg_color, bg=bg_color, activebackground=activebg_color, font=((button_font)), command=lambda : click(3))
+b4 = Button(root, text="4", padx=button_padx, pady=button_pady, fg=fg_color, bg=bg_color, activebackground=activebg_color, font=((button_font)), command=lambda : click(4))
+b5 = Button(root, text="5", padx=button_padx, pady=button_pady, fg=fg_color, bg=bg_color, activebackground=activebg_color, font=((button_font)), command=lambda : click(5))
+b6 = Button(root, text="6", padx=button_padx, pady=button_pady, fg=fg_color, bg=bg_color, activebackground=activebg_color, font=((button_font)), command=lambda : click(6))
+b7 = Button(root, text="7", padx=button_padx, pady=button_pady, fg=fg_color, bg=bg_color, activebackground=activebg_color, font=((button_font)), command=lambda : click(7))
+b8 = Button(root, text="8", padx=button_padx, pady=button_pady, fg=fg_color, bg=bg_color, activebackground=activebg_color, font=((button_font)), command=lambda : click(8))
+b9 = Button(root, text="9", padx=button_padx, pady=button_pady, fg=fg_color, bg=bg_color, activebackground=activebg_color, font=((button_font)), command=lambda : click(9))
 
-bplus = Button(root, text="+", padx=button_padx, pady=button_pady, fg="white", bg="#383B3F", activebackground="#211F22", font=((button_font)), command=plus)
-bminus = Button(root, text="-", padx=button_padx, pady=button_pady, fg="white", bg="#383B3F", activebackground="#211F22", font=((button_font)), command=minus)
-bmult = Button(root, text="X", padx=button_padx, pady=button_pady, fg="white", bg="#383B3F", activebackground="#211F22", font=((button_font)), command=mult)
-bdiv = Button(root, text="/", padx=button_padx, pady=button_pady, fg="white", bg="#383B3F", activebackground="#211F22", font=((button_font)), command=div)
-bclear = Button(root, text="C", padx=button_padx, pady=button_pady, fg="white", bg="#383B3F", activebackground="#211F22", font=((button_font)), command=clear)
-bequal = Button(root, text="=", padx=button_padx, pady=button_pady, fg="white", bg="#505A61", activebackground="#211F22", font=((button_font)), command=equal)
+bplus = Button(root, text="+", padx=button_padx, pady=button_pady, fg=fg_color, bg=bg_color, activebackground=activebg_color, font=((button_font)), command=plus)
+bminus = Button(root, text="-", padx=button_padx, pady=button_pady, fg=fg_color, bg=bg_color, activebackground=activebg_color, font=((button_font)), command=minus)
+bmult = Button(root, text="X", padx=button_padx, pady=button_pady, fg=fg_color, bg=bg_color, activebackground=activebg_color, font=((button_font)), command=mult)
+bdiv = Button(root, text="/", padx=button_padx, pady=button_pady, fg=fg_color, bg=bg_color, activebackground=activebg_color, font=((button_font)), command=div)
+bclear = Button(root, text="C", padx=button_padx, pady=button_pady, fg=fg_color, bg=bg_color, activebackground=activebg_color, font=((button_font)), command=clear)
+bequal = Button(root, text="=", padx=button_padx, pady=button_pady, fg=fg_color, bg="#505A61", activebackground=activebg_color, font=((button_font)), command=equal)
 
 #Buttons Positioning
 
-bclear.grid(row=4, column=0, padx=5, pady=5)
-b0.grid(row=4, column=1, padx=5, pady=5)
-bequal.grid(row=4, column=2, padx=5, pady=5)
-bplus.grid(row=4, column=3, padx=5, pady=5)
+btn_padx = 5
+btn_pady = 5
 
-b1.grid(row=3, column=0, padx=5, pady=5)
-b2.grid(row=3, column=1, padx=5, pady=5)
-b3.grid(row=3, column=2, padx=5, pady=5)
-bminus.grid(row=3, column=3, padx=5, pady=5)
+bclear.grid(row=4, column=0, padx=btn_padx, pady=btn_pady)
+b0.grid(row=4, column=1, padx=btn_padx, pady=btn_pady)
+bequal.grid(row=4, column=2, padx=btn_padx, pady=btn_pady)
+bplus.grid(row=4, column=3, padx=btn_padx, pady=btn_pady)
 
-b4.grid(row=2, column=0, padx=5, pady=5)
-b5.grid(row=2, column=1, padx=5, pady=5)
-b6.grid(row=2, column=2, padx=5, pady=5)
-bmult.grid(row=2, column=3, padx=5, pady=5)
+b1.grid(row=3, column=0, padx=btn_padx, pady=btn_pady)
+b2.grid(row=3, column=1, padx=btn_padx, pady=btn_pady)
+b3.grid(row=3, column=2, padx=btn_padx, pady=btn_pady)
+bminus.grid(row=3, column=3, padx=btn_padx, pady=btn_pady)
 
-b7.grid(row=1, column=0, padx=5, pady=5)
-b8.grid(row=1, column=1, padx=5, pady=5)
-b9.grid(row=1, column=2, padx=5, pady=5)
-bdiv.grid(row=1, column=3, padx=5, pady=5)
+b4.grid(row=2, column=0, padx=btn_padx, pady=btn_pady)
+b5.grid(row=2, column=1, padx=btn_padx, pady=btn_pady)
+b6.grid(row=2, column=2, padx=btn_padx, pady=btn_pady)
+bmult.grid(row=2, column=3, padx=btn_padx, pady=btn_pady)
+
+b7.grid(row=1, column=0, padx=btn_padx, pady=btn_pady)
+b8.grid(row=1, column=1, padx=btn_padx, pady=btn_pady)
+b9.grid(row=1, column=2, padx=btn_padx, pady=btn_pady)
+bdiv.grid(row=1, column=3, padx=btn_padx, pady=btn_pady)
 
 
 root.mainloop()
